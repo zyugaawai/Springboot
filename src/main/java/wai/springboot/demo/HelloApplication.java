60622b04d0a8cf6cdf32b065e07f78458cbc369d
zyugaawai <869353984@qq.com>
2018-07-15 20:56:38 +0800

    1.增加

--- src/main/java/wai/springboot/demo/HelloApplication.java
+++ src/main/java/wai/springboot/demo/HelloApplication.java
@@ -1,8 +1,12 @@
 package wai.springboot.demo;
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,8 +19,16 @@ public class HelloApplication {
 	@RequestMapping("hello")
 	@ResponseBody
 	public String sayHello() {
-		return "hello world";
+		return "hello world你好";
 	}
+	
+	//消息转换器，默认utf-8
+//	@Bean
+//	public StringHttpMessageConverter stringHttpMessageConverter() {
+//		StringHttpMessageConverter converter = 
+//				new StringHttpMessageConverter(Charset.forName("ISO-8859-1"));
+//		return converter;
+//	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
